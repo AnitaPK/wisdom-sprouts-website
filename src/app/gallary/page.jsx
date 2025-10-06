8a9c8f7c606211370a5f3bf675a7575931e73d16
AnitaPK <anita.khamitkar@gmail.com>
2025-10-06 17:56:58 +0530

    gallary started

---
+++ src/app/gallary/page.jsx
@@ -0,0 +1,13 @@
+import GallaryPage from '@/components/Gallary/GallaryPage'
+import React from 'react'
+import '@/styles/globals.css'
+
+const page = () => {
+  return (
+    <>
+      <GallaryPage />
+    </>
+  )
+}
+
+export default page
---
+++ src/components/Gallary/GallaryPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GallaryPage = () => {
+  return (
+    <div>
+      <h1>Gallary</h1>
+    </div>
+  )
+}
+
+export default GallaryPage
