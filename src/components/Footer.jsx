9bcc66bf66951519bc3bd00e980d2b288880add5
AnitaPK <anita.khamitkar@gmail.com>
2025-10-06 19:57:19 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ import {
   FaMapMarkerAlt,
   FaEnvelope,
   FaPhoneAlt,
-  FaLinkedinIn ,
-  FaTwitter ,
+  FaLinkedinIn,
+  FaTwitter,
   FaInstagram,
   FaYoutube,
-  FaFacebookF
+  FaFacebookF,
 } from "react-icons/fa";
 import "./Footer.css";
 import Image from "next/image";
@@ -21,9 +21,9 @@ const Footer = () => {
         <Row className="text-center text-md-start">
           {/* Left Column */}
           <Col md={4} className="mb-4">
-            <Image 
-              src='/logo.png'
-              alt='wisdom sprouts'
+            <Image
+              src="/logo.png"
+              alt="wisdom sprouts"
               width={200}
               height={111}
             />
@@ -47,36 +47,83 @@ const Footer = () => {
           {/* Get in Touch */}
           <Col md={3} className="mb-4">
             <h6 className="fw-bold ">Get In Touch</h6>
-            <p className="custom-get-in-touch"><FaMapMarkerAlt className="me-2 custom-icon-color" />406, White Square, Wakad-Hinjawadi Road, Pune, Maharashtra 411045</p>
-            <p className="custom-get-in-touch"><FaEnvelope className="me-2 custom-icon-color" />helpdesk@wisdomsprouts.in</p>
-            <p className="custom-get-in-touch"><FaPhoneAlt className="me-2 custom-icon-color" />+91-7770043825</p>
-            <p className="custom-get-in-touch"><FaPhoneAlt className="me-2 custom-icon-color" />+91-7770023825</p>
-
+            <p className="custom-get-in-touch">
+              <FaMapMarkerAlt className="me-2 custom-icon-color" />
+              406, White Square, Wakad-Hinjawadi Road, Pune, Maharashtra 411045
+            </p>
+            <p className="custom-get-in-touch">
+              <FaEnvelope className="me-2 custom-icon-color" />
+              helpdesk@wisdomsprouts.in
+            </p>
+            <p className="custom-get-in-touch">
+              <FaPhoneAlt className="me-2 custom-icon-color" />
+              +91-7770043825
+            </p>
+            <p className="custom-get-in-touch">
+              <FaPhoneAlt className="me-2 custom-icon-color" />
+              +91-7770023825
+            </p>
           </Col>
 
           {/* Social Media */}
           <Col md={2} className="mb-4">
             <h6 className="fw-bold">Follow us on</h6>
-            <div className="row g-3 w-25">
-              <div className="col">
-              <a href="https://in.linkedin.com/company/wisdom-sprouts-training-and-consulting-solutions">
-              <span className="custom-icon-social">
-                <Image src="/social-Icons/LinkdIn.png" width={13} height={13} alt='linkedIn image' /></span>
+            <div className="footer-social-grid ">
+              <a
+                href="https://in.linkedin.com/company/wisdom-sprouts-training-and-consulting-solutions"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="social-link"
+              >
+                <Image
+                  src="/social-Icons/LinkdIn.png"
+                  width={14}
+                  height={14}
+                  alt="LinkedIn"
+                />
               </a>
-              <a href="https://www.instagram.com/wisdomsprouts/?hl=en">
-                <span className="custom-icon-social"><Image src="/social-Icons/Instagram.png" width={13} height={13} alt='Instagram image' /></span>
-              </a>
-              </div>
-              <div className="col">
 
-              <a href="https://www.facebook.com/wisdomsprouts/">
-                <span className="custom-icon-social"><Image src="/social-Icons/Facebook.png" width={13} height={13} alt='Facebook image' /></span>
+              <a
+                href="https://www.instagram.com/wisdomsprouts/?hl=en"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="social-link"
+              >
+                <Image
+                  src="/social-Icons/Instagram.png"
+                  width={14}
+                  height={14}
+                  alt="Instagram"
+                />
+              </a>
 
+              <a
+                href="https://www.facebook.com/wisdomsprouts/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="social-link"
+              >
+                <Image
+                  src="/social-Icons/Facebook.png"
+                  width={14}
+                  height={14}
+                  alt="Facebook"
+                />
               </a>
-              <a href="https://www.youtube.com/@wisdomsprouts1276">
-                <span className="custom-icon-social"><Image src="/social-Icons/Youtube.png" width={13} height={13} alt='youtube image' /></span>
+
+              <a
+                href="https://www.youtube.com/@wisdomsprouts1276"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="social-link"
+              >
+                <Image
+                  src="/social-Icons/Youtube.png"
+                  width={14}
+                  height={14}
+                  alt="YouTube"
+                />
               </a>
-              </div>
             </div>
           </Col>
         </Row>
@@ -87,11 +134,14 @@ const Footer = () => {
         <Row>
           <Col md={6} className="">
             <p className="mb-0">
-              Copyright © 2025 Wisdom Sprouts IT Training Hub. All rights reserved.
+              Copyright © 2025 Wisdom Sprouts IT Training Hub. All rights
+              reserved.
             </p>
           </Col>
           <Col md={6} className="text-md-end">
-            <p className="mb-0">Hinjawadi | Akurdi | Sinhagad Road | Tilak Road</p>
+            <p className="mb-0">
+              Hinjawadi | Akurdi | Sinhagad Road | Tilak Road
+            </p>
           </Col>
         </Row>
       </Container>
