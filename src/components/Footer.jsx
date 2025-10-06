4cc91734268691afc29555fcb3dc89cc1077c652
AnitaPK <anita.khamitkar@gmail.com>
2025-10-06 19:47:44 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,14 +20,14 @@ const Footer = () => {
       <Container>
         <Row className="text-center text-md-start">
           {/* Left Column */}
-          <Col md={3} className="mb-4">
+          <Col md={4} className="mb-4">
             <Image 
               src='/logo.png'
               alt='wisdom sprouts'
               width={200}
               height={111}
             />
-            <p className="custom-info-ws">
+            <p className="custom-info-ws mt-4">
               Wisdom Sprouts IT Training Hub is a leading IT training provider
               institute in Pune. Wisdom Sprouts is owned by ScaleFull
               Technologies LLP.
@@ -48,16 +48,17 @@ const Footer = () => {
           <Col md={3} className="mb-4">
             <h6 className="fw-bold ">Get In Touch</h6>
             <p className="custom-get-in-touch"><FaMapMarkerAlt className="me-2 custom-icon-color" />406, White Square, Wakad-Hinjawadi Road, Pune, Maharashtra 411045</p>
-            <p><FaEnvelope className="me-2 custom-icon-color" />helpdesk@wisdomsprouts.in</p>
-            <p><FaPhoneAlt className="me-2 custom-icon-color" />+91-7770043825</p>
-            <p><FaPhoneAlt className="me-2 custom-icon-color" />+91-7770023825</p>
+            <p className="custom-get-in-touch"><FaEnvelope className="me-2 custom-icon-color" />helpdesk@wisdomsprouts.in</p>
+            <p className="custom-get-in-touch"><FaPhoneAlt className="me-2 custom-icon-color" />+91-7770043825</p>
+            <p className="custom-get-in-touch"><FaPhoneAlt className="me-2 custom-icon-color" />+91-7770023825</p>
 
           </Col>
 
           {/* Social Media */}
-          <Col md={3} className="mb-4">
+          <Col md={2} className="mb-4">
             <h6 className="fw-bold">Follow us on</h6>
-            <div className="d-flex justify-content-center justify-content-md-start gap-3">
+            <div className="row g-3 w-25">
+              <div className="col">
               <a href="https://in.linkedin.com/company/wisdom-sprouts-training-and-consulting-solutions">
               <span className="custom-icon-social">
                 <Image src="/social-Icons/LinkdIn.png" width={13} height={13} alt='linkedIn image' /></span>
@@ -65,17 +66,17 @@ const Footer = () => {
               <a href="https://www.instagram.com/wisdomsprouts/?hl=en">
                 <span className="custom-icon-social"><Image src="/social-Icons/Instagram.png" width={13} height={13} alt='Instagram image' /></span>
               </a>
+              </div>
+              <div className="col">
 
               <a href="https://www.facebook.com/wisdomsprouts/">
                 <span className="custom-icon-social"><Image src="/social-Icons/Facebook.png" width={13} height={13} alt='Facebook image' /></span>
 
               </a>
-              <a href="#">
-                <span className="custom-icon-social"><Image src="/social-Icons/Twitter.png" width={13} height={13} alt='Twitter image' /></span>
-                  </a>
               <a href="https://www.youtube.com/@wisdomsprouts1276">
                 <span className="custom-icon-social"><Image src="/social-Icons/Youtube.png" width={13} height={13} alt='youtube image' /></span>
               </a>
+              </div>
             </div>
           </Col>
         </Row>
